afe5f7b4b1610c4b40170dd857cfc8966b954f1d
CaPloy <chutimon1104@gmail.com>
2023-04-19 16:07:24 +0700

    Create Exercise4_Chutimon_S.py

---
+++ Exercise4_Chutimon_S.py
@@ -0,0 +1,10 @@
+print("--- Your Score ---")
+a=15
+b=20000
+c=145
+d=16.5
+print("Foundation English :", a)
+print("General Business :", b)
+print("Introduction to Computer Systems :", c)
+print("Computer Programming :", d)
+print("--- GOOD LUCK ---")
