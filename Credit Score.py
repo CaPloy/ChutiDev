9cafb84cd6ccfb44a281a10f7656f76c71d9284e
CaPloy <chutimon1104@gmail.com>
2023-04-24 17:53:51 +0700

    Create Credit Score.py

---
+++ Credit Score.py
@@ -0,0 +1,21 @@
+score = int(input("Your Score"))
+if score >=80:
+    print("A")
+elif score >=75:
+    print("B+")
+elif score >=70:
+    print("B")
+elif score >=65:
+    print("C+")
+elif score >=60:
+    print("C")
+elif score >=55:
+    print("D+")
+elif score >=50:
+    print("D")
+else :
+    print("F")
+    
+
+    
+    
