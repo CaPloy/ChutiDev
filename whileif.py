c16a950850d68481f565a6530be331a67f6e453a
CaPloy <chutimon1104@gmail.com>
2023-05-13 21:30:17 +0700

    Create whileif.py

---
+++ whileif.py
@@ -0,0 +1,7 @@
+usernameinput = input("Username:")
+passwordinput = input("Password:")
+while (usernameinput != "admin" or passwordinput != "567"):
+    usernameinput = input("Username:")
+    passwordinput = input("Password:")
+print("Again!")
+    
