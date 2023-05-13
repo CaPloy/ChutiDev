bcddd548000176d16762fdc40537737c95c9f8f1
CaPloy <chutimon1104@gmail.com>
2023-05-13 21:29:56 +0700

    Create tuple.py

---
+++ tuple.py
@@ -0,0 +1,6 @@
+tupleExample = ('pan','ploy','peung')
+print(tupleExample)
+tupleExample2 = ('Boboo','paoum')
+tupleExample3 = tupleExample+tupleExample2
+print(tupleExample3)
+print(tupleExample3[:2])
