dfab74913e5199a0b04545ee290bde84ff6c68b0
CaPloy <chutimon1104@gmail.com>
2023-05-13 21:29:42 +0700

    Create for loop2.py

---
+++ for loop2.py
@@ -0,0 +1,10 @@
+#calculator
+inputRound = int(input("Please Enter Number:"))
+sum = 0
+print(list(range(inputRound)))
+for i in range(inputRound):
+    inputNumber = int(input("i"+str(i+1)+":"))
+    
+    
+    
+    
