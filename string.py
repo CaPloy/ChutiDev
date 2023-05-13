84d567fe7cb79b32f951eb3c15c7a32227ddee97
CaPloy <chutimon1104@gmail.com>
2023-05-13 21:30:14 +0700

    Create string.py

---
+++ string.py
@@ -0,0 +1,11 @@
+name = input("Firstname :")
+lastname = input("Lastname :")
+print("Hello %s %s !!"%(name.capitalize,lastname))
+
+#cappitalize คือฟังชั่นที่ทำให้ตัวอักศรตัวแรกสุดเป็นพิมพ์ใหญ๋ //มีมาจากเครื่องอยู่แล้ว
+
+text = "Ploy"
+textFormatted = "Wellcome %s"%(text)
+print(textFormatted.center(21,"-"))
+
+#center คือฟังชั่นที่ทำให้อยู่ตำแหน่งตรงกลาง
